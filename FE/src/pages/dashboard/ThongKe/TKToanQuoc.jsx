8c99293b0987e12f4db12ef5bbfb04697354ca8b
Shiro Hien <thaohien0607@gmail.com>
2023-12-30 09:29:22 +0700

    add analytic cards
    config in TKToanQuoc and AnalyticEcommerce which is in the same folder

--- FE/src/pages/dashboard/ThongKe/TKToanQuoc.jsx
+++ FE/src/pages/dashboard/ThongKe/TKToanQuoc.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react'
+import AnalyticEcommerce from './AnalyticEcommerce'
 
 // material-ui
 import { useTheme } from '@mui/material/styles'
+import { Grid, Typography } from '@mui/material'
+import WarehouseIcon from '@mui/icons-material/Warehouse'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import PeopleOutlineRoundedIcon from '@mui/icons-material/PeopleOutlineRounded'
 
 // third-party
 import ReactApexChart from 'react-apexcharts'
@@ -142,6 +147,35 @@ const TKToanQuoc = () => {
   return (
     <div id="chart">
       <h3>THỐNG KÊ TRÊN TOÀN QUỐC</h3>
+      <Grid container rowSpacing={4.0} columnSpacing={2.75}>
+        <Grid item xs={12} sx={{ mb: -2 }}>
+          <Typography variant="h5">Dashboard</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} md={2} lg={3}>
+          <AnalyticEcommerce
+            title="Điểm tập kết"
+            count="2"
+            icon={() => <WarehouseIcon style={{ color: '#DC143C' }} />}/>
+        </Grid>
+        <Grid item xs={12} sm={6} md={2} lg={3}>
+          <AnalyticEcommerce
+            title="Điểm giao dịch"
+            count="5"
+            icon={() => <WarehouseIcon style={{ color: '#EEE732' }} />}/>
+        </Grid>
+        <Grid item xs={12} sm={6} md={2} lg={3}>
+          <AnalyticEcommerce
+            title="Đơn hàng"
+            count="7"
+            icon={() => <AddShoppingCartIcon style={{ color: '#00CC00' }} />}/>
+        </Grid>
+        <Grid item xs={12} sm={6} md={2} lg={3}>
+          <AnalyticEcommerce
+            title="Nhân viên"
+            count="30"
+            icon={() => <PeopleOutlineRoundedIcon style={{ color: '#FF9933' }} />}/>
+        </Grid>
+      </Grid>
       <ReactApexChart options={options} series={series} type="bar" height={430} />
     </div>
   )
